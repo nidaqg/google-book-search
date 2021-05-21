36806796c83146b3fee5e1cff4c0df1e4458124d
nqg <nidaghuman@gmail.com>
2021-05-21 15:52:47 -0400

    depoly to heroku

--- server.js
+++ server.js
@@ -15,7 +15,14 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}
+);
 
 
 // Start the API server
